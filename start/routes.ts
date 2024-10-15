045947d7b3a2fa2f87c38bc5da2ce11b6ffd2e8a
capthndsme <nieoy@msn.com>
2024-10-15 15:20:13 +0000

    fixup confs

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,22 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+
+/**
+ * AUTH ROUTE GROUP.
+ */
+router.group(() => {
+
+})
+.use(middleware.auth)
+
+
+router.post('/api/login', () => false)
